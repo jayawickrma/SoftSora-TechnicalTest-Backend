c720a780ac3a22faa2ac9ab56ddacd7edb453da6
jayawickrama <nishantharaka2004@gmail.com>
2025-06-02 00:51:56 +0530

    mongoDb connected

--- Server.ts
+++ Server.ts
@@ -1,11 +1,17 @@
 import express from 'express'
 import dotenv  =require("dotenv");
+import mainRouter from "./SoftSora-Technical/Routes/MainRouter";
 const app =express();
+import {connectDB} from "./SoftSora-Technical/Config/db";
 
 dotenv.config()
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
+connectDB()
+// mongodb://localhost:27017/
+
+app.use('/api/v1',mainRouter.router);
 
 const port =process.env.PORT || 8080 ;
 app.listen(port,()=>{
