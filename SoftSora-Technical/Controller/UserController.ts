335d590b890a993498c128053436af682939a1b2
jayawickrama <nishantharaka2004@gmail.com>
2025-06-02 15:47:19 +0530

    created user controller methods

--- SoftSora-Technical/Controller/UserController.ts
+++ SoftSora-Technical/Controller/UserController.ts
@@ -1,24 +1,38 @@
 import {UserDTO} from "../DTO/UserDTO";
+import {createUser, finsByEmail} from "../Service/UserService";
+import jwt, {Secret} from "jsonwebtoken";
 
 class UserController{
     async signIn(req:any,resp:any){
         const email =req.body.email;
-        const pw  =req.body.password;
+        const password  =req.body.password;
+        const name = req.body.name
 
-        const user:UserDTO ={email,pw}
+        const user:UserDTO = {email,password,name};
 
         try{
-            const verified =
+            const verified = await finsByEmail(user);
+                if (verified){
+                    const token =jwt.sign({email},process.env.SECRET_KEY as Secret,{expiresIn:"30m"});
+                    const refreshToken =jwt.sign({email},process.env.REFRESH_TOKEN as Secret,{expiresIn:"7d"});
+                    resp.json({accessToken:token,refreshToken:refreshToken});
+                }
 
         }catch (err){
-            resp.status(401).json("UnAuthorized user..")
+            resp.sendStatus(403).send("Invalid Credentials...")
         }
 
     }
 
     async signUp(req:any,resp:any){
+        const user:UserDTO =req.body;
 
-
+        try{
+            await createUser(user);
+            resp.status(201).json("User Account Created..." ,user)
+        }catch (err){
+            resp.status(401).json('UnAuthorized user and cant log into the System ..')
+        }
     }
 }
 export default UserController;
